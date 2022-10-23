c5ce296ca3a4ace772367e8457b0ea82dc01ce07
Nazar <nazariykharchuk@gmail.com>
2022-10-23 17:00:20 +0300

    Added function №2 ("findLastIndex") to the second lab

--- Lab2/src/Lab2.purs
+++ Lab2/src/Lab2.purs
@@ -1,8 +1,8 @@
 module Lab2 where
 
 import Prelude
-import Data.List (List(..), (:))
-import Data.Maybe (Maybe(..))
+import Data.List (List(..), (:), reverse, length)
+import Data.Maybe (Maybe(..), isNothing, fromMaybe)
 
 findIndex :: forall a. (a -> Boolean) -> List a -> Maybe Int
 findIndex f list = recursiveFindIndex f list 0
@@ -12,3 +12,8 @@ findIndex f list = recursiveFindIndex f list 0
             | (function x) = (Just integer)
             | otherwise = recursiveFindIndex f xs (integer + 1)
         recursiveFindIndex function Nil integer = Nothing
+
+findLastIndex :: forall a. (a -> Boolean) -> List a -> Maybe Int
+findLastIndex f list = 
+    let findedIndex = (findIndex f (reverse list)) in
+    if (isNothing findedIndex) then Nothing else Just ((length list) - 1 - (fromMaybe 0 findedIndex))
--- Lab2/src/Main.purs
+++ Lab2/src/Main.purs
@@ -16,3 +16,9 @@ main = do
   log $ show $ Lab2.findIndex (\x -> x >= 2) (1 : 2 : 3 : 4 : Nil)
   log "Function \"findIndex\" with arguments: (\\x -> x > 5) (1 : 2 : 3 : 4 : 5 : Nil)"
   log $ show $ Lab2.findIndex (\x -> x > 5) (1 : 2 : 3 : 4 : 5 : Nil)
+
+  log "\tFunction #2"
+  log "Function \"findLastIndex\" with arguments: (\\x -> x >= 2) (1 : 2 : 3 : 4 : Nil)"
+  log $ show $ Lab2.findLastIndex (\x -> x >= 2) (1 : 2 : 3 : 4 : Nil)
+  log "Function \"findLastIndex\" with arguments: (\\x -> x > 5) (1 : 2 : 3 : 4 : 5 : Nil)"
+  log $ show $ Lab2.findLastIndex (\x -> x > 5) (1 : 2 : 3 : 4 : 5 : Nil)
